e6b65e1b6e98af18beaa3035e77fffbbe0adbb88
NewtonSilva <newtonsilva@users.noreply.github.com>
2022-07-21 12:17:34 +0930

    atoi

--- libft.h
+++ libft.h
@@ -27,4 +27,12 @@ void    *ft_memcpy(void *dest, const void *src, size_t n)
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 int ft_strlen(const char *str)
 int ft_toupper(int i)
+int	ft_toupper(int i)
+char	*ft_strchr(const char *s, int i)
+char	*ft_strrchr(const char *s, int c)
+int	ft_strncmp(const char *s1, char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+int	ft_atoi(const char *str)
+
 #endif
